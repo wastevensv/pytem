19d98df3e30cd2a75fce8fe010431e7ccbdf8fa2
William Stevens <contact@wastevensv.com>
2015-08-03 16:06:39 -0400

    Fixed run error. Exits on missing args.

--- pytem.py
+++ pytem.py
@@ -108,4 +108,5 @@ def main(templatedir, indir, outdir):
 if __name__== "__main__":
   if len(sys.argv) != 4: # print usage instructions if any parameters are missing.
     print("Usage: %s <templatedir> <indir> <outdir>"%sys.argv[0])
+    sys.exit(-1)
   main(sys.argv[1],sys.argv[2],sys.argv[3])
